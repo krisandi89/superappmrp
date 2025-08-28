191438b29588ff1602ea2572929b3a111c922356
krisandi89 <krisandi89@gmail.com>
2025-08-28 14:03:40 +0700

    hapusRekap.js

---
+++ api/hapusRekap.js
@@ -0,0 +1,25 @@
+// /api/hapusRekap.js
+import { kv } from '@vercel/kv';
+
+export default async function handler(request, response) {
+  if (request.method !== 'DELETE') {
+    return response.status(405).json({ message: 'Metode tidak diizinkan' });
+  }
+
+  try {
+    const { username, number } = request.body;
+
+    if (!username || !number) {
+      return response.status(400).json({ success: false, message: 'Username dan Nomor Rekap diperlukan.' });
+    }
+
+    const key = `${username}-${number}`;
+    const result = await kv.del(key);
+
+    if (result > 0) {
+        return response.status(200).json({ success: true, message: 'Rekap berhasil dihapus.' });
+    } else {
+        return response.status(404).json({ success: false, message: 'Rekap tidak ditemukan untuk dihapus.' });
+    }
+
+  } catch (err
